a8a0d9b30f7fc7f5b315bdaebb801530e68dc0f7
aaron-matto-cko <aaron.matto@checkout.com>
2025-09-20 15:44:50 +0100

    latest;

--- scripts/mazes/recursiveDivision.js
+++ scripts/mazes/recursiveDivision.js
@@ -9,6 +9,7 @@ const xLowerBoundary = 1;
 const xHigherBoundary = 58;
 const yLowerBoundary = 1;
 const yHigherBoundary = 18;
+const yBoundaries = [0, 19]
 
 export const recursiveDivision = async (yEnd, yStart, xEnd, xStart) => {
 
@@ -32,20 +33,24 @@ export const recursiveDivision = async (yEnd, yStart, xEnd, xStart) => {
 const createVerticalWall = () => {
 
   // random x coord between 2 and 57
-
-  var startPointX = randomPoint(2, 57);
+  var startPointX = randomPoint(2, 58);
 
   var wallDivs = Array.from(document.querySelectorAll(`[data-x='${startPointX}']`));
 
 
-  wallDivs.forEach(wall => {
+  wallDivs.forEach(wallDiv => {
 
+    var validExtensionPoints = []
 
-    var validationResult = validateWallSegment(wall, true);
+    var validationResult = validateVerticalWallSegment(wallDiv);
 
     if (validationResult == true) {
-      wall.classList.add('wall-node');
+      validExtensionPoints.add(wall);
     };
+
+    if (validExtensionPoints.length == 0) {
+      // no valid w
+    }
 
   });
 
@@ -71,9 +76,8 @@ const randomPoint = (inclusiveStart, exclusiveEnd) => {
   return Math.floor(Math.random() * (exclusiveEnd)) + inclusiveStart;
 };
 
-const validateWallSegment = (currentCell, isVertical) => {
+const validateVerticalWallSegment = (currentCell) => {
 
-  if (isVertical) {
 
     var currentX = currentCell.getAttribute('data-x');
     var currentY = currentCell.getAttribute('data-y');
@@ -87,12 +91,16 @@ const validateWallSegment = (currentCell, isVertical) => {
     var canExtendLeft = false;
     var canExtendRight = false;
 
-    if (leftNeighbour.classList.contains('wall-node') == false && cellTwoToLeft.classList.contains('wall-node') == false) {
+    if (leftNeighbour.classList.contains('wall-node') == false 
+    && cellTwoToLeft.classList.contains('wall-node') == false
+    && yBoundaries.includes(currentY) == false) {
       // valid extension point for left
       canExtendLeft = true;
     };
 
-    if (rightNeighbour.classList.contains('wall-node') == false && cellTwoToRight.classList.contains('wall-node') == false) {
+    if (rightNeighbour.classList.contains('wall-node') == false 
+    && cellTwoToRight.classList.contains('wall-node') == false
+    && yBoundaries.includes(currentY) == false) {
       // valid extension point for right
       canExtendRight = true;
     };
@@ -102,8 +110,6 @@ const validateWallSegment = (currentCell, isVertical) => {
       return false;
     };
 
-
-  };
 
 
 
@@ -164,3 +170,8 @@ class ExtensionPoint {
 
 }
 
+// if current wall extends into another wall:
+// - the wall that is extended into will have an id (key) and that will will have a list of valid extension points (value).
+// The extension point will be removed from the list since the current wall has extended into it
+// - extension points on the current wall cannot be within 2 squares of the wall the current wall extended from AND
+// the wall extended into
